53e53bebf105eaef4eb75260a3db4cebb355bb59
mrm <matt.rmoore@gmail.com>
2021-02-24 12:21:55 -0600

    Course 5 complete thru Chapter 3

--- Track02_Python_Data_Science/Course05_Intro_Matplotlib_Visualizations/Chapter03/Section02/Cr05_Ch03_Sn02_Ex02.py
+++ Track02_Python_Data_Science/Course05_Intro_Matplotlib_Visualizations/Chapter03/Section02/Cr05_Ch03_Sn02_Ex02.py
@@ -1,14 +1,14 @@
 fig, ax = plt.subplots()
+
 # Plot a histogram of "Weight" for mens_rowing
-ax.hist(mens_rowing["Weight"])
+ax.hist(mens_rowing["Weight"], label="Rowing", histtype="step", bins=5)
 
 # Compare to histogram of "Weight" for mens_gymnastics
-ax.hist(mens_gymnastics["Weight"])
+ax.hist(mens_gymnastics["Weight"], label="Gymnastics", histtype="step", bins=5)
 
-# Set the x-axis label to "Weight (kg)"
 ax.set_xlabel("Weight (kg)")
-
-# Set the y-axis label to "# of observations"
 ax.set_ylabel("# of observations")
 
+# Add the legend and show the Figure
+ax.legend()
 plt.show()
